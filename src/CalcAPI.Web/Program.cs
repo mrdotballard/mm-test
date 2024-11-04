29470df7ccdaedca52ead3ffc73281376a6155a0
Matthew Ballard <mr.ballard@gmail.com>
2024-11-04 23:25:20 +0000

    Add security definition for swagger

--- src/CalcAPI.Web/Program.cs
+++ src/CalcAPI.Web/Program.cs
@@ -2,6 +2,7 @@ using CalcAPI.Infrastructure.Extensions;
 using CalcAPI.Application.Extensions;
 using System.Text.Json;
 using CalcAPI.Web.Middleware;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,15 +16,42 @@ builder.Services.AddControllers();
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "Calculator API", Version = "v1" });
+
+        // Add security definition
+        c.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme
+        {
+            Type = SecuritySchemeType.ApiKey,
+            Name = "ApiKey",
+            In = ParameterLocation.Header,
+            Description = "API Key authentication using the 'X-API-Key' header"
+        });
+
+        // Add security requirement
+        c.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "ApiKey"
+                    }
+                },
+                new string[] {}
+            }
+        });
+    }
+);
+
 
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
-
-// Add early in the pipeline, before other middleware 
-app.UseErrorHandling();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -31,6 +59,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// Add early in the pipeline, before other middleware 
+app.UseErrorHandling();
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
